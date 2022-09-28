94f0b490dfc961689796868359c004bd01093f3f
Gatlin Carrier <gatlin.carrier@apiture.com>
2022-09-28 08:13:51 -0700

    renamed some files

--- android/app/src/main/java/com/fiservpoc/MyModule.java
+++ android/app/src/main/java/com/fiservpoc/MyModule.java
@@ -1,6 +1,6 @@
 package com.fiservpoc;
-import android.util.Log;
 import android.content.Intent;
+import android.util.Log;
 import androidx.annotation.NonNull;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -8,25 +8,38 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
 import com.fiserv.dps.mobile.sdk.bridge.controller.Bridge;
-//import com.fiserv.dps.mobile.sdk.
-//Implementing GenericTag for session timeout
-public class MyModule extends ReactContextBaseJavaModule  {
+import com.fiserv.dps.mobile.sdk.interfaces.GenericTag;
+
+
+public class MyModule extends ReactContextBaseJavaModule implements GenericTag {
+
     ReactApplicationContext context = getReactApplicationContext();
-    public MyModule(@NonNull ReactApplicationContext reactContext) { super(reactContext);
+
+    public MyModule(@NonNull ReactApplicationContext reactContext) {
+        super(reactContext);
     }
-    @NonNull @Override public String getName() {
+
+    @NonNull
+    @Override
+    public String getName() {
         return "MyModule";
     }
+
     Callback callback;
-    @ReactMethod public void NavigateToZelle(String appName, String baseURL, String instId, String product, String ssoKey, ReadableMap parameters, ReadableMap pd, Callback callback){
+
+    @ReactMethod
+    public void NavigateToZelle(String appName, String baseURL, String instId, String product, String ssoKey, ReadableMap parameters, ReadableMap pd, Callback callback) {
+
         this.callback = callback;
-//        Bridge.genericTag = this;
 
-        Intent intent = new Intent(context, LaunchZelleActivity.class);
+        Bridge.genericTag = this;
 
-        if(intent.resolveActivity(context.getPackageManager()) != null){
+        Intent intent = new Intent(context, LaunchZelleActivity.class); //error: cannot find symbol Intent intent = new Intent(context, LaunchZelleActivity.class);
+
+        if (intent.resolveActivity(context.getPackageManager()) != null) {
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            Log.d("Data from ui ======>", parameters.toHashMap()+" "+pd.toHashMap()); intent.putExtra("appName", appName);
+            Log.d("Data from ui ======>", parameters.toHashMap() + " " + pd.toHashMap());
+            intent.putExtra("appName", appName);
             intent.putExtra("baseURL", baseURL);
             intent.putExtra("instId", instId);
             intent.putExtra("product", product);
@@ -35,11 +48,16 @@ public class MyModule extends ReactContextBaseJavaModule  {
             intent.putExtra("pd", pd.toHashMap());
             context.startActivity(intent);
         }
+    }
 
-//        @Override public void sessionTag(@NonNull String s) {//override method for session timeout
-//            if (!s.equals("")){ try {
-//                callback.invoke(s); }catch (Exception e){
-//                Log.e("ReactNative", "Caught Exception: " + e.getMessage()); }
-//            } }
+    @Override
+    public void sessionTag(@NonNull String s) {
+        if (!s.equals("")){
+            try {
+                callback.invoke(s);
+        }  catch (Exception e){
+                Log.e("ReactNative", "Caught Exception: " + e.getMessage());
+            }
+        }
     }
 }
